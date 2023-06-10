3dea0b66cb2b54e1fb5334157f0bf235d9b4a225
siddesh761941 <siddesh976@gmail.com>
2023-06-10 17:20:05 +0530

    13

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,6 @@ const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const cors_1 = __importDefault(require("cors"));
 const mongo_connect_utils_1 = __importDefault(require("./utils/mongo-connect.utils"));
 const common_utils_1 = require("./utils/common.utils");
 const deserializeuser_1 = __importDefault(require("./middlewares/deserializeuser"));
@@ -32,10 +31,11 @@ app.use(deserializeuser_1.default);
 //     origin: ["https://e-phsing.firebaseapp.com/", "https://e-phsing.web.app/"]
 //   })
 // );
-const corsOpts = {
-    origin: "*"
-};
-app.use((0, cors_1.default)(corsOpts));
+app.all("*", function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 const mongoConnection = () => __awaiter(void 0, void 0, void 0, function* () {
     const db = yield (0, common_utils_1.fetchRetry)(5, mongo_connect_utils_1.default.connectToDB);
     if (db) {
--- src/index.ts
+++ src/index.ts
@@ -23,11 +23,11 @@ app.use(deserializeUser);
 //   })
 // );
 
-const corsOpts = {
-  origin: "*"
-};
-
-app.use(cors(corsOpts));
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 const mongoConnection = async () => {
   const db = await fetchRetry(5, mongoUtil.connectToDB);
